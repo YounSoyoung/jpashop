489a0aa86166847eaaed05629b2e00e75a316139
YounSoyoung <grace902853@gmail.com>
2023-09-17 16:25:46 +0900

    카테고리와 상품 연관관계 설정

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,8 +1,12 @@
 package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
+import jpabook.jpashop.domain.Category;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)   //상속 클래스 전략을 부모 클래스에서 지정해준다
@@ -18,4 +22,7 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
 }
