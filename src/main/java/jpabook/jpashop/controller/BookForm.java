6944d842dd120a5eb4d2ed13f3fe65d78ca04e5f
YounSoyoung <grace902853@gmail.com>
2023-09-20 17:17:27 +0900

    상품 등록

--- src/main/java/jpabook/jpashop/controller/BookForm.java
+++ src/main/java/jpabook/jpashop/controller/BookForm.java
@@ -1,3 +1,18 @@
 package jpabook.jpashop.controller;
 
-public class BookForm {}
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class BookForm {
+    private Long id;
+
+    //상품 공통 속성
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    //책만의 속성
+    private String author;
+    private String isbn;
+}
