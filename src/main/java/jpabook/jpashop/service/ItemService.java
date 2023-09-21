3e22cf8c19a23cd23fb03448cb0f3113a1513653
YounSoyoung <grace902853@gmail.com>
2023-09-21 14:26:06 +0900

    변경 감지와 병합

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +19,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, String name, int price, int stockQuantity){
+        Item findItem = itemRepository.findOne(itemId);     //id를 기반으로 실제 DB에 있는 영속성 엔티티를 찾아온다.
+        findItem.setPrice(price);    //찾은 영속성 엔티티에 param으로 받은 책의 필드값들을 설정해준다.
+        findItem.setName(name);
+        findItem.setStockQuantity(stockQuantity);
+        //스프링의 @Transactional에 의해 트랜젝션이 커밋된다. -> JPA가 flush를 날린다. => 영속성 엔티티인 findItem의 변경을 감지한 후 업데이트 처리
+
     }
 
     public List<Item> findItems() {
